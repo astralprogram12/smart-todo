64a8365b87c6b3ee5fba263941450086d7504562
doremifas0l <nindigiriindah@gmail.com>
2025-09-03 20:03:22 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import FeaturesPage from './pages/FeaturesPage'
 import AuthCallback from './pages/AuthCallback'
@@ -24,7 +23,6 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<SimplifiedLoginPage />} />
-            <Route path="/login/original" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/features" element={<FeaturesPage />} />
             <Route path="/auth/callback" element={<AuthCallback />} />
